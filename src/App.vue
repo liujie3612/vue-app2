f1311b66efda4e3b77822914f41bfc754b711208
liujie3612 <liujie@wilddog.com>
2017-02-16 00:25:27 +0800

    finish

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    
+    <keep-alive>
+      <router-view :seller="seller"></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+
+<script type="text/ecmascript-6">
+  import urlParse from 'common/script/util';
+  import header from './components/header/header.vue';
+
+  const ERR_OK = 0
+
+  export default{
+    data() {
+      return {
+        seller: {
+          //id的值是自执行的函数 返回url里的id的值
+          type:Object,
+          id:(() => {
+            let queryParam = urlParse();
+            return queryParam.id
+          })()
+        }
+      }
+    },
+    created() {
+      this.$http.get('/api/seller?id=' + this.seller.id).then((response) => {
+        /*返回的response 是 promise 对象*/
+        response = response.body;
+        if( response.errno === ERR_OK ) {
+            // this.seller = response.data;
+            //给对象添加属性  否则的话id的属性会被新的对象覆盖  相当于合并对象
+            this.seller = Object.assign({},this.seller,response.data)
+        }
+
+      });
+    },
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus'>
+    @import './common/styles/mixin';
+    .tab
+      display: flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      border-1px(rgba(7,17,27,0.1))
+      .tab-item
+        flex: 1
+        text-align: center
+        & > a
+          display:inline-block
+          width:100%
+          font-size: 14px
+          color: rgb(77,85,93)
+          &.active
+            color:rgb(240,20,20)
+</style>
